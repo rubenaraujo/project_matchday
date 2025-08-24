b81062d4e35cffb7fa515152c5ab969cb0143e65
Ruben Araujo (CTW) <Ruben.Araujo@ctw.bmwgroup.com>
2025-08-24 17:24:30 +0100

    Add workflows that download data
    Signed-off-by: Ruben Araujo (CTW) <Ruben.Araujo@ctw.bmwgroup.com>

--- .github/workflows/rss2json.js
+++ .github/workflows/rss2json.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
-const { DOMParser } = require('xmldom');
+const xml2js = require('xml2js');
 
 const xml = fs.readFileSync('zapping.xml', 'utf8');
-const parser = new DOMParser();
-const xmlDoc = parser.parseFromString(xml, 'application/xml');
+const parser = new xml2js.Parser();
 
-const items = Array.from(xmlDoc.getElementsByTagName('item')).map(item => {
-  const getText = tag => {
-    const el = item.getElementsByTagName(tag)[0];
-    return el ? el.textContent : '';
-  };
-  return {
-    title: getText('title'),
-    link: getText('link'),
-    description: getText('description'),
-    pubDate: getText('pubDate'),
-    guid: getText('guid')
-  };
+parser.parseString(xml, (err, result) => {
+  if (err) {
+    console.error('Error parsing XML:', err);
+    process.exit(1);
+  }
+  // Find items in RSS structure
+  let items = [];
+  if (result.rss && result.rss.channel && result.rss.channel[0].item) {
+    items = result.rss.channel[0].item.map(item => ({
+      title: item.title ? item.title[0] : '',
+      link: item.link ? item.link[0] : '',
+      description: item.description ? item.description[0] : '',
+      pubDate: item.pubDate ? item.pubDate[0] : '',
+      guid: item.guid ? item.guid[0]._ || item.guid[0] : ''
+    }));
+  } else {
+    console.error('No items found in XML. Structure:', JSON.stringify(result, null, 2));
+  }
+  fs.writeFileSync('zapping.json', JSON.stringify({ items }, null, 2));
 });
-
-fs.writeFileSync('zapping.json', JSON.stringify({ items }, null, 2));

c59deea77870b017497c8b7ec3518aacf5132f54
Rúben Araújo <ruben.araujo@live.com>
2025-08-29 15:00:35 +0100

    Atualizar o rss2json.js

--- .github/workflows/rss2json.js
+++ .github/workflows/rss2json.js
@@ -1,9 +1,14 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+
+const xml = fs.readFileSync('zapping.xml', 'utf8');
+const parser = new xml2js.Parser();
+
 parser.parseString(xml, (err, result) => {
   if (err) {
     console.error('Error parsing XML:', err);
     process.exit(1);
   }
-  
   // Find items in RSS structure
   let items = [];
   if (result.rss && result.rss.channel && result.rss.channel[0].item) {
@@ -17,24 +22,5 @@ parser.parseString(xml, (err, result) => {
   } else {
     console.error('No items found in XML. Structure:', JSON.stringify(result, null, 2));
   }
-  
-  // Criar o JSON como string
-  const jsonString = JSON.stringify({ items }, null, 2);
-  
-  // Escrever usando stream para ter mais controle
-  const writeStream = fs.createWriteStream('zapping.json', { 
-    encoding: 'utf8',
-    flags: 'w'
-  });
-  
-  writeStream.write(jsonString, 'utf8');
-  writeStream.end();
-  
-  writeStream.on('finish', () => {
-    console.log('Arquivo JSON criado com sucesso');
-  });
-  
-  writeStream.on('error', (err) => {
-    console.error('Erro ao escrever arquivo:', err);
-  });
+  fs.writeFileSync('zapping.json', JSON.stringify({ items }, null, 2));
 });

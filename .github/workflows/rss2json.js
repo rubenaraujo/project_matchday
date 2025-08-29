d189358161bc2b17b36a357181f03e684a7ca3af
Rúben Araújo <ruben.araujo@live.com>
2025-08-29 14:53:36 +0100

    Atualizar o rss2json.js

--- .github/workflows/rss2json.js
+++ .github/workflows/rss2json.js
@@ -1,26 +1,50 @@
-const fs = require('fs');
-const xml2js = require('xml2js');
+const fs = require(‘fs’);
+const xml2js = require(‘xml2js’);
 
-const xml = fs.readFileSync('zapping.xml', 'utf8');
+// Abordagem alternativa: ler o arquivo como buffer e depois converter
+const xmlBuffer = fs.readFileSync(‘zapping.xml’);
+const xml = xmlBuffer.toString(‘utf8’);
+
+// Parser mais simples
 const parser = new xml2js.Parser();
 
 parser.parseString(xml, (err, result) => {
-  if (err) {
-    console.error('Error parsing XML:', err);
-    process.exit(1);
-  }
-  // Find items in RSS structure
-  let items = [];
-  if (result.rss && result.rss.channel && result.rss.channel[0].item) {
-    items = result.rss.channel[0].item.map(item => ({
-      title: item.title ? item.title[0] : '',
-      link: item.link ? item.link[0] : '',
-      description: item.description ? item.description[0] : '',
-      pubDate: item.pubDate ? item.pubDate[0] : '',
-      guid: item.guid ? item.guid[0]._ || item.guid[0] : ''
-    }));
-  } else {
-    console.error('No items found in XML. Structure:', JSON.stringify(result, null, 2));
-  }
-  fs.writeFileSync('zapping.json', JSON.stringify({ items }, null, 2), 'utf8');
+if (err) {
+console.error(‘Error parsing XML:’, err);
+process.exit(1);
+}
+
+// Find items in RSS structure
+let items = [];
+if (result.rss && result.rss.channel && result.rss.channel[0].item) {
+items = result.rss.channel[0].item.map(item => ({
+title: item.title ? item.title[0] : ‘’,
+link: item.link ? item.link[0] : ‘’,
+description: item.description ? item.description[0] : ‘’,
+pubDate: item.pubDate ? item.pubDate[0] : ‘’,
+guid: item.guid ? item.guid[0]._ || item.guid[0] : ‘’
+}));
+} else {
+console.error(‘No items found in XML. Structure:’, JSON.stringify(result, null, 2));
+}
+
+// Criar o JSON como string
+const jsonString = JSON.stringify({ items }, null, 2);
+
+// Escrever usando stream para ter mais controle
+const writeStream = fs.createWriteStream(‘zapping.json’, {
+encoding: ‘utf8’,
+flags: ‘w’
+});
+
+writeStream.write(jsonString, ‘utf8’);
+writeStream.end();
+
+writeStream.on(‘finish’, () => {
+console.log(‘Arquivo JSON criado com sucesso’);
+});
+
+writeStream.on(‘error’, (err) => {
+console.error(‘Erro ao escrever arquivo:’, err);
+});
 });
